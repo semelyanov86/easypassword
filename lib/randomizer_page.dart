bd7efa375547a98b755955aa5eeef57c9c738698
Sergey <sergey86@gmail.com>
2021-09-28 08:43:06 +0300

    Implemented state hooks

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:random_password_generator/random_password_generator.dart';
 
-class RandomizerPage extends StatefulWidget {
+class RandomizerPage extends HookWidget {
+  final password = RandomPasswordGenerator();
+
   final int length;
 
   final bool uppercase, lowercase, symbols, numbers;
 
-  const RandomizerPage({
+  RandomizerPage({
     Key? key,
     required this.length,
     required this.uppercase,
@@ -16,39 +19,28 @@ class RandomizerPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return RandomizerPageState();
-  }
-}
-
-class RandomizerPageState extends State<RandomizerPage> {
-  String? _generatedPass;
-
-  final password = RandomPasswordGenerator();
-
-  @override
   Widget build(BuildContext context) {
+    final generatedPass = useState<String?>(null);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Password Randomizer'),
       ),
       body: Center(
         child: Text(
-          _generatedPass ?? 'Generate a password',
+          generatedPass.value ?? 'Generate a password',
           style: TextStyle(fontSize: 42),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text('Generate Password'),
         onPressed: () => {
-          setState(() => {
-                _generatedPass = password.randomPassword(
-                    passwordLength: widget.length.toDouble(),
-                    letters: widget.lowercase,
-                    uppercase: widget.uppercase,
-                    numbers: widget.numbers,
-                    specialChar: widget.symbols)
-              })
+          generatedPass.value = password.randomPassword(
+              passwordLength: length.toDouble(),
+              letters: lowercase,
+              uppercase: uppercase,
+              numbers: numbers,
+              specialChar: symbols)
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/settings_selector_page.dart
+++ lib/settings_selector_page.dart
@@ -1,66 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:pass_generator/randomizer_page.dart';
 import 'package:pass_generator/settings_form.dart';
 
 import 'initial_data_settings.dart';
 
-class SettingsSelectorPage extends StatefulWidget {
+class SettingsSelectorPage extends HookWidget {
+  final formKey = GlobalKey<FormState>();
+
   SettingsSelectorPage({Key? key}) : super(key: key);
 
   @override
-  _SettingsSelectorPageState createState() => _SettingsSelectorPageState();
-}
-
-class _SettingsSelectorPageState extends State<SettingsSelectorPage> {
-  final formKey = GlobalKey<FormState>();
-
-  int _length = 8;
-
-  bool _uppercase = true;
-
-  bool _lowercase = true;
-
-  bool _symbols = false;
-
-  bool _numbers = true;
-
-  _SettingsSelectorPageState() {
-    this._numbers = InitialDataSettings.numbers;
-    this._lowercase = InitialDataSettings.lowercase;
-    this._symbols = InitialDataSettings.symbols;
-    this._uppercase = InitialDataSettings.uppercase;
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final length = useState<int>(8);
+
+    final uppercase = useState<bool>(true);
+
+    final lowercase = useState<bool>(true);
+
+    final symbols = useState<bool>(false);
+
+    final numbers = useState<bool>(true);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Choose Settings for Generator'),
       ),
       body: SettingsForm(
         formKey: formKey,
-        intValueSetter: (value) => _length = value,
-        uppercaseValueSetter: (value) => {
-          InitialDataSettings.uppercase = value,
-          setState(() => _uppercase = value)
-        },
-        lowercaseValueSetter: (value) => {
-          InitialDataSettings.lowercase = value,
-          setState(() => _lowercase = value)
-        },
-        symbolsValueSetter: (value) => {
-          InitialDataSettings.symbols = value,
-          setState(() => _symbols = value)
-        },
-        numbersValueSetter: (value) => {
-          InitialDataSettings.numbers = value,
-          setState(() => _numbers = value)
-        },
+        intValueSetter: (value) => length.value = value,
+        uppercaseValueSetter: (value) =>
+            {InitialDataSettings.uppercase = value, uppercase.value = value},
+        lowercaseValueSetter: (value) =>
+            {InitialDataSettings.lowercase = value, lowercase.value = value},
+        symbolsValueSetter: (value) =>
+            {InitialDataSettings.symbols = value, symbols.value = value},
+        numbersValueSetter: (value) =>
+            {InitialDataSettings.numbers = value, numbers.value = value},
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward),
         onPressed: () => {
-          if (!_numbers && !_symbols && !_uppercase && !_lowercase)
+          if (!numbers.value &&
+              !symbols.value &&
+              !uppercase.value &&
+              !lowercase.value)
             {
               showDialog<String>(
                 context: context,
@@ -81,11 +65,11 @@ class _SettingsSelectorPageState extends State<SettingsSelectorPage> {
               formKey.currentState?.save(),
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => RandomizerPage(
-                      length: _length,
-                      uppercase: _uppercase,
-                      lowercase: _lowercase,
-                      symbols: _symbols,
-                      numbers: _numbers)))
+                      length: length.value,
+                      uppercase: uppercase.value,
+                      lowercase: lowercase.value,
+                      symbols: symbols.value,
+                      numbers: numbers.value)))
             }
         },
       ),
